02c73cf39dbf4fb6ee6bf3431094a7b3942d0382
Zubkov Vova <vova.zubkov@gmail.com>
2015-03-17 19:45:34 +0300

    data methods

--- js/calendar.new.js
+++ js/calendar.new.js
@@ -5,7 +5,7 @@
 var calendar = calendar || {},
     data = JSON.parse(localStorage.getItem("calendar"));
 
-    data = data || {};
+data = data || {};
 
 (function (calendar, data) {
 
@@ -18,6 +18,7 @@ var calendar = calendar || {},
             clTitle: '.month-year-title',
             clRow: '.cl-table-row',
             clCol: '.cl-table-col',
+            form: 'editEvent',
             dayList: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
             monthList: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
             todayDate: new Date(),
@@ -26,16 +27,8 @@ var calendar = calendar || {},
         tempData = {
             id: '',
             title: '',
-            date: {
-                day:'',
-                month:'',
-                year:''
-            },
-            time:{
-                hour:'',
-                minute:''
-            },
-            participants:'',
+            date: '',
+            participants: '',
             description: ''
         };
 
@@ -48,61 +41,66 @@ var calendar = calendar || {},
         };
     })();
 
-    function todayCheck ( curDate, todayDate, col ){
-        if ( curDate.getDate() == todayDate.getDate() &&
+    function todayCheck(curDate, todayDate, col) {
+        if (curDate.getDate() == todayDate.getDate() &&
             curDate.getMonth() == todayDate.getMonth() &&
-            curDate.getFullYear() == todayDate.getFullYear())
-        {
+            curDate.getFullYear() == todayDate.getFullYear()) {
             col.classList.add('today')
         }
-        else if (col.classList.contains('today'))
-        {
+        else if (col.classList.contains('today')) {
             col.classList.remove('today')
         }
     }
 
-    function clDraw ( curDate, todayDate, data ) {
+    function clDraw(curDate, todayDate, data) {
         curDate.setDate(curDate.getDate() - curDate.getDay() + 1);
         document.querySelector(genVar.clTitle).innerHTML = curDate.getMonthName() + ' ' + curDate.getFullYear();
         [].slice.call(document.querySelectorAll(genVar.clRow)).forEach(function (row, i) {
             [].slice.call(row.querySelectorAll(genVar.clCol)).forEach(function (col, j) {
                 col.querySelector('.day-of-month').innerHTML = '' + genVar.curDate.getDate();
-                //for(var x in data)
-                //{
-                //    if ( parseInt(data[x].date.day,10) == parseInt(curDate.getDay(), 10) &&
-                //        parseInt(data[x].date.month,10) == parseInt(curDate.getMonthName(), 10) &&
-                //        parseInt(data[x].date.year,10) == parseInt(curDate.getFullYear(), 10))
-                //    {
-                //        console.log(curDate.getTime());
-                        col.setAttribute('date-id', '' + parseInt(curDate.getTime(), 10));
-                //    }
-                //}
-                todayCheck ( curDate, todayDate, col );
+                col.setAttribute('date',curDate.getFullYear()  + ', ' + curDate.getMonth() + ', ' + curDate.getDate());
+                col.classList.remove('has-event');
+                col.querySelector('.event-title').innerHTML = '';
+                col.querySelector('.event-participants').innerHTML = '';
+                col.querySelector('.event-description').innerHTML = '';
+
+                var checkDate = new Date(col.getAttribute('date')).getTime();
+                data = JSON.parse(localStorage.getItem("calendar"));
+
+                    for (var x in data)
+                    {
+                        console.log();
+                        if (data[x].id == checkDate)
+                        {
+                            col.querySelector('.event-title').innerHTML = String(data[x].title);
+                            col.querySelector('.event-participants').innerHTML = String(data[x].participants);
+                            col.querySelector('.event-description').innerHTML = String(data[x].description);
+                            col.classList.add('has-event');
+                        }
+                    }
+
+                todayCheck(curDate, todayDate, col);
                 curDate.setDate(curDate.getDate() + 1);
             })
         });
     }
 
-    function hdBlockPosition (curBlock, elTrigger, clBlock){
+    function hdBlockPosition(curBlock, elTrigger, clBlock) {
         if ((curBlock.offsetWidth + elTrigger.offsetLeft + elTrigger.offsetWidth)
-            > document.querySelector(clBlock).offsetWidth)
-        {
+            > document.querySelector(clBlock).offsetWidth) {
             curBlock.classList.remove('right');
             curBlock.classList.add('left');
             curBlock.style.left = elTrigger.offsetLeft - curBlock.offsetWidth - 20 + 'px';
         }
-        else
-        {
+        else {
             curBlock.classList.remove('left');
             curBlock.classList.add('right');
             curBlock.style.left = elTrigger.offsetLeft + elTrigger.offsetWidth + 20 + 'px';
         }
-        if (elTrigger.offsetTop == 0)
-        {
+        if (elTrigger.offsetTop == 0) {
             curBlock.style.top = elTrigger.offsetTop + 'px';
         }
-        else if ((curBlock.offsetHeight + elTrigger.offsetTop) > document.querySelector(clBlock).offsetHeight)
-        {
+        else if ((curBlock.offsetHeight + elTrigger.offsetTop) > document.querySelector(clBlock).offsetHeight) {
             curBlock.classList.remove('top');
             curBlock.classList.add('bottom');
             var i = 0;
@@ -111,32 +109,30 @@ var calendar = calendar || {},
                 i++;
             } while (curBlock.offsetHeight + curBlock.offsetTop + 20 < document.querySelector(clBlock).offsetHeight);
         }
-        else
-        {
+        else {
             curBlock.classList.remove('bottom');
             curBlock.classList.add('top');
             curBlock.style.top = elTrigger.offsetTop - 20 + 'px';
         }
     }
 
-    function hdBlockClose (){
+    function hdBlockClose() {
         [].slice.call(document.querySelectorAll('.' + genVar.trigger))
-            .forEach(function (elt, i) {elt.classList.remove('active');});
+            .forEach(function (elt, i) {
+                elt.classList.remove('active');
+            });
         [].slice.call(document.querySelectorAll('.' + genVar.block))
-            .forEach(function (elb, i) {elb.classList.remove('visible');});
+            .forEach(function (elb, i) {
+                elb.classList.remove('visible');
+            });
     }
 
-    function hdBlockOpen (elTrigger, curBlock, clBlock){
-        hdBlockClose ();
-        setTimeout(function () {
-            elTrigger.classList.add('active');
-            curBlock.classList.add('visible');
-            if (elTrigger.getAttribute(genVar.trigger) == 'add-edit-ev')
-                hdBlockPosition (curBlock, elTrigger, clBlock);
-        },50)
+    function hdBlockOpen(elTrigger, curBlock) {
+        elTrigger.classList.add('active');
+        curBlock.classList.add('visible');
     }
 
-    function eventHandling (trigger, block, close, clBlock) {
+    function eventHandling(trigger, block, close, clBlock) {
         var triggerList = [].slice.call(document.querySelectorAll('.' + trigger));
 
         triggerList.forEach(function (elTrigger, i) {
@@ -145,9 +141,28 @@ var calendar = calendar || {},
 
             elTrigger.addEventListener('click', function (e) {
                 e.preventDefault();
-                    if (!elTrigger.classList.contains('active')) hdBlockOpen(elTrigger, curBlock, clBlock);
-                    else hdBlockClose();
-                });
+                if (!elTrigger.classList.contains('active')) {
+                    hdBlockClose();
+                    setTimeout(function () {
+                        hdBlockOpen(elTrigger, curBlock);
+                    }, 50);
+                    if (elTrigger.getAttribute(genVar.trigger) == 'add-edit-ev')
+                        var form = document.getElementById(genVar.form),
+                            date = new Date(elTrigger.getAttribute('date')),
+                            dateString = elTrigger.getAttribute('date').split(', '),
+                            fDate = form.getElementsByClassName(genVar.form + '-date');
+
+                    dateString[1] = genVar.monthList[dateString[1]];
+                    fDate[0].setAttribute('placeholder', String(dateString[2] + ', ' + dateString[1] + ', ' + dateString[0]));
+
+                    data = JSON.parse(localStorage.getItem("calendar"));
+                    console.log(data);
+                    hdBlockPosition(curBlock, elTrigger, clBlock);
+                }
+                else {
+                    hdBlockClose();
+                }
+            });
             closeList.forEach(function (elClose, j) {
                 elClose.addEventListener('click', function (e) {
                     e.preventDefault();
@@ -172,15 +187,26 @@ var calendar = calendar || {},
 
     calendar.init = function () {
         genVar.todayDate = new Date();
-        genVar.curDate = new Date(genVar.todayDate.getFullYear() + ', ' + (genVar.todayDate.getMonth()+1) + ', ' + genVar.todayDate.getDate());
-        //tempData.id = genVar.todayDate.getTime();
-        //data[tempData.id] = tempData;
-        //localStorage.setItem("calendar", JSON.stringify(data));
-        data = JSON.parse(localStorage.getItem("calendar"));
-        //localStorage.clear();
-        //console.log(data);
+        genVar.curDate = new Date(genVar.todayDate.getFullYear() + ', '
+        + (genVar.todayDate.getMonth() + 1) + ', '
+        + genVar.todayDate.getDate());
 
-        clDraw ( genVar.curDate, genVar.todayDate, data);
+        localStorage.setItem("calendar", JSON.stringify(data));
+        data = JSON.parse(localStorage.getItem("calendar"));
+
+        clDraw(genVar.curDate, genVar.todayDate, data);
+    };
+
+    calendar.clearStart = function () {
+        genVar.todayDate = new Date();
+        genVar.curDate = new Date(genVar.todayDate.getFullYear() + ', '
+        + (genVar.todayDate.getMonth() + 1) + ', '
+        + genVar.todayDate.getDate());
+        data = {};
+        localStorage.setItem("calendar", JSON.stringify(data));
+        data = JSON.parse(localStorage.getItem("calendar"));
+
+        clDraw(genVar.curDate, genVar.todayDate, data);
     };
 
     calendar.movePrev = function () {
@@ -190,16 +216,44 @@ var calendar = calendar || {},
             dt++;
         } while (dt < 70);
         data = JSON.parse(localStorage.getItem("calendar"));
-        clDraw ( genVar.curDate, genVar.todayDate, data);
+        clDraw(genVar.curDate, genVar.todayDate, data);
     };
 
     calendar.moveNext = function () {
         data = JSON.parse(localStorage.getItem("calendar"));
-        clDraw ( genVar.curDate, genVar.todayDate, data);
+        clDraw(genVar.curDate, genVar.todayDate, data);
     };
 
     calendar.addEvent = function () {
+        var col = document.querySelector('.' + genVar.trigger + '.active'),
+            form = document.getElementById(genVar.form),
+            date = new Date(col.getAttribute('date'));
+
+        tempData.id = date.getTime();
+
+        if (form.getElementsByClassName(genVar.form + '-title')[0].value)
+        tempData.title = form.getElementsByClassName(genVar.form + '-title')[0].value;
+        else tempData.title ='Заголовок пуст';
+
+        if (form.getElementsByClassName(genVar.form + '-date')[0].value)
+        tempData.date = form.getElementsByClassName(genVar.form + '-date')[0].value;
+        else tempData.date = String(form.getElementsByClassName(genVar.form + '-date')[0].getAttribute('placeholder'));
+
+        tempData.participants = form.getElementsByClassName(genVar.form + '-participants')[0].value;
+        tempData.description = form.getElementsByClassName(genVar.form + '-description')[0].value;
+
+        data[tempData.id] = tempData;
+
+        localStorage.setItem("calendar", JSON.stringify(data));
         hdBlockClose();
+
+        data = JSON.parse(localStorage.getItem("calendar"));
+        var dt = 0;
+        do {
+            genVar.curDate.setDate(genVar.curDate.getDate() - 1);
+            dt++;
+        } while (dt < 35);
+        clDraw(genVar.curDate, genVar.todayDate, data);
     };
 
     calendar.addEventShort = function () {
@@ -207,9 +261,13 @@ var calendar = calendar || {},
     };
 
     calendar.removeEvent = function () {
+        var col = document.querySelector('.' + genVar.trigger + '.active'),
+            form = document.getElementById(genVar.form),
+            date = new Date(col.getAttribute('date'));
+
         hdBlockClose();
     };
 
-    eventHandling (genVar.trigger, genVar.block, genVar.close, genVar.clBlock);
+    eventHandling(genVar.trigger, genVar.block, genVar.close, genVar.clBlock);
 
 })(calendar, data);
