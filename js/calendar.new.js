a2e89fd0e36d914845b192d384168490723e60d9
Zubkov Vova <vova.zubkov@gmail.com>
2015-03-18 04:01:03 +0300

    update calendar.new.js

--- js/calendar.new.js
+++ js/calendar.new.js
@@ -18,9 +18,10 @@ data = data || {};
             clTitle: '.month-year-title',
             clRow: '.cl-table-row',
             clCol: '.cl-table-col',
-            form: 'editEvent',
+            editForm: 'editEvent',
+            addForm: 'addEvent',
             dayList: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-            monthList: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+            monthList: ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'],
             todayDate: new Date(),
             curDate: new Date()
         },
@@ -30,6 +31,17 @@ data = data || {};
             date: '',
             participants: '',
             description: ''
+        },
+        editForm = {
+            body: document.getElementById(genVar.editForm),
+            title: document.getElementsByClassName(genVar.editForm + '-title'),
+            date: document.getElementsByClassName(genVar.editForm + '-date'),
+            participants: document.getElementsByClassName(genVar.editForm + '-participants'),
+            description: document.getElementsByClassName(genVar.editForm + '-description')
+        },
+        addForm = {
+            body: document.getElementById(genVar.addForm),
+            title: document.getElementsByClassName(genVar.addForm + '-title')
         };
 
     (function () {
@@ -41,7 +53,15 @@ data = data || {};
         };
     })();
 
-    function todayCheck(curDate, todayDate, col) {
+    function find (arr, value) {
+        var tmp = arr.length;
+        while (tmp--) {
+            if (arr[tmp] == value) return tmp;
+        }
+        return false;
+    }
+
+    function todayCheck (curDate, todayDate, col) {
         if (curDate.getDate() == todayDate.getDate() &&
             curDate.getMonth() == todayDate.getMonth() &&
             curDate.getFullYear() == todayDate.getFullYear()) {
@@ -52,7 +72,22 @@ data = data || {};
         }
     }
 
-    function clDraw(curDate, todayDate, data) {
+    function findColDate (date) {
+        [].slice.call(document.querySelectorAll(genVar.clRow)).forEach(function (row, i) {
+            [].slice.call(row.querySelectorAll(genVar.clCol)).forEach(function (col, j) {
+                if (col.classList.contains('active')) {
+                    var dt = 0;
+                    do {
+                        date.setDate(date.getDate() + 1);
+                        dt++;
+                    } while (dt < (i * 7) + j);
+
+                }
+            })
+        });
+    }
+
+    function clDraw (curDate, todayDate) {
         curDate.setDate(curDate.getDate() - curDate.getDay() + 1);
         document.querySelector(genVar.clTitle).innerHTML = curDate.getMonthName() + ' ' + curDate.getFullYear();
         [].slice.call(document.querySelectorAll(genVar.clRow)).forEach(function (row, i) {
@@ -63,12 +98,12 @@ data = data || {};
                 col.querySelector('.event-participants').innerHTML = '';
                 col.querySelector('.event-description').innerHTML = '';
 
-                var checkDate = curDate.getTime();
+                var checkDate = new Date(curDate.getTime());
+                    checkDate = checkDate.getTime();
                 data = JSON.parse(localStorage.getItem("calendar"));
 
                 for (var x in data) {
-                    if (data[x].id == parseInt(checkDate,10)) {
-                        console.log('1');
+                    if (data[x].id == parseInt(checkDate, 10)) {
                         col.querySelector('.event-title').innerHTML = data[x].title.toString();
                         col.querySelector('.event-participants').innerHTML = data[x].participants.toString();
                         col.querySelector('.event-description').innerHTML = data[x].description.toString();
@@ -86,7 +121,7 @@ data = data || {};
         } while (dt < 35);
     }
 
-    function hdBlockPosition(curBlock, elTrigger, clBlock) {
+    function hdBlockPosition (curBlock, elTrigger, clBlock) {
         if ((curBlock.offsetWidth + elTrigger.offsetLeft + elTrigger.offsetWidth)
             > document.querySelector(clBlock).offsetWidth) {
             curBlock.classList.remove('right');
@@ -117,7 +152,7 @@ data = data || {};
         }
     }
 
-    function hdBlockClose() {
+    function hdBlockClose () {
         [].slice.call(document.querySelectorAll('.' + genVar.trigger))
             .forEach(function (elt, i) {
                 elt.classList.remove('active');
@@ -137,14 +172,19 @@ data = data || {};
                         eli.value = '';
                     });
             });
+        editForm.title[0].removeAttribute('readonly');
+        editForm.date[0].removeAttribute('readonly');
+        editForm.participants[0].removeAttribute('readonly');
+        editForm.description[0].removeAttribute('readonly');
     }
 
-    function hdBlockOpen(elTrigger, curBlock) {
+    function hdBlockOpen (elTrigger, curBlock) {
         elTrigger.classList.add('active');
         curBlock.classList.add('visible');
+
     }
 
-    function eventHandling(trigger, block, close, clBlock) {
+    function eventHandling (trigger, block, close, clBlock) {
         var triggerList = [].slice.call(document.querySelectorAll('.' + trigger));
 
         triggerList.forEach(function (elTrigger, i) {
@@ -157,31 +197,62 @@ data = data || {};
                     hdBlockClose();
                     setTimeout(function () {
                         hdBlockOpen(elTrigger, curBlock);
-                    }, 50);
-                    if (elTrigger.getAttribute(genVar.trigger) == 'add-edit-ev')
-                    {
-                        var form = document.getElementById(genVar.form),
-                            fDate = form.getElementsByClassName(genVar.form + '-date'),
-                            date = new Date(genVar.curDate.getTime());
+                        if (elTrigger.getAttribute(genVar.trigger) == 'add-edit-ev') {
+                            var date = new Date(genVar.curDate.getTime());
+                            findColDate(date);
+                            var checkDate = date.getTime();
+                            data = JSON.parse(localStorage.getItem("calendar"));
+                            editForm.date[0].setAttribute('placeholder', (date.getDate() + ', ' + date.getMonthName() + ', ' + date.getFullYear()).toString());
+                            for (var x in data) {
+                                if (data[x].id == parseInt(checkDate, 10)) {
+                                    if (data[x].title.toString() == ''){
+                                        editForm.title[0].removeAttribute('readonly');
+                                    }
+                                    else {
+                                        editForm.title[0].value = data[x].title.toString();
+                                        editForm.title[0].readOnly = true;
+                                        editForm.title[0].addEventListener('click',function (){
+                                            this.removeAttribute('readonly');
+                                        });
+                                    }
 
-                        [].slice.call(document.querySelectorAll(genVar.clRow)).forEach(function (row, i) {
-                            [].slice.call(row.querySelectorAll(genVar.clCol)).forEach(function (col, j) {
-                                if (col.classList.contains('active')){
-                                    var dt = 0;
-                                    do {
-                                        date.setDate(date.getDate() + 1);
-                                        dt++;
-                                    } while (dt < (i*7)+j);
-                                    fDate[0].setAttribute('placeholder', (date.getDate() + ', ' + date.getMonthName() + ', ' + date.getFullYear()).toString());
+                                    if (data[x].date.toString() == ''){
+                                        editForm.date[0].removeAttribute('readonly');
+                                    }
+                                    else {
+                                        editForm.date[0].value = data[x].date.toString();
+                                        editForm.date[0].readOnly = true;
+                                        editForm.date[0].addEventListener('click',function (){
+                                            this.removeAttribute('readonly');
+                                        });
+                                    }
+
+                                    if (data[x].participants.toString() == ''){
+                                        editForm.participants[0].removeAttribute('readonly');
+                                    }
+                                    else {
+                                        editForm.participants[0].value = data[x].participants.toString();
+                                        editForm.participants[0].readOnly = true;
+                                        editForm.participants[0].addEventListener('click',function (){
+                                            this.removeAttribute('readonly');
+                                        });
+                                    }
+
+                                    if (data[x].description.toString() == ''){
+                                        editForm.description[0].removeAttribute('readonly');
+                                    }
+                                    else {
+                                        editForm.description[0].value = data[x].description.toString();
+                                        editForm.description[0].readOnly = true;
+                                        editForm.description[0].addEventListener('click',function (){
+                                            this.removeAttribute('readonly');
+                                        });
+                                    }
                                 }
-                            })
-                        });
-
-
-                        data = JSON.parse(localStorage.getItem("calendar"));
-                        //console.log(data);
-                        hdBlockPosition(curBlock, elTrigger, clBlock);
-                    }
+                            }
+                            hdBlockPosition(curBlock, elTrigger, clBlock);
+                        }
+                    }, 50);
                 }
                 else {
                     hdBlockClose();
@@ -208,28 +279,21 @@ data = data || {};
         });
     }
 
-    calendar.init = function () {
-        genVar.todayDate = new Date();
-        genVar.curDate = new Date(genVar.todayDate.getFullYear() + ', '
-        + (genVar.todayDate.getMonth() + 1) + ', '
-        + genVar.todayDate.getDate());
-
-        localStorage.setItem("calendar", JSON.stringify(data));
+    calendar.onStart = function () {
+        genVar.curDate = new Date(genVar.todayDate.getTime());
         data = JSON.parse(localStorage.getItem("calendar"));
-
-        clDraw(genVar.curDate, genVar.todayDate, data);
+        clDraw(genVar.curDate, genVar.todayDate);
     };
 
-    calendar.clearStart = function () {
+    calendar.init = function () {
         genVar.todayDate = new Date();
-        genVar.curDate = new Date(genVar.todayDate.getFullYear() + ', '
-        + (genVar.todayDate.getMonth() + 1) + ', '
-        + genVar.todayDate.getDate());
+        genVar.todayDate = new Date(genVar.todayDate.getFullYear() + ', ' + (genVar.todayDate.getMonth()+1) + ', ' + genVar.todayDate.getDate());
+        genVar.curDate = new Date(genVar.todayDate.getTime());
         data = {};
         localStorage.setItem("calendar", JSON.stringify(data));
         data = JSON.parse(localStorage.getItem("calendar"));
         console.clear();
-        clDraw(genVar.curDate, genVar.todayDate, data);
+        clDraw(genVar.curDate, genVar.todayDate);
     };
 
     calendar.movePrev = function () {
@@ -239,7 +303,7 @@ data = data || {};
             dt++;
         } while (dt < 35);
         data = JSON.parse(localStorage.getItem("calendar"));
-        clDraw(genVar.curDate, genVar.todayDate, data);
+        clDraw(genVar.curDate, genVar.todayDate);
     };
 
     calendar.moveNext = function () {
@@ -249,82 +313,85 @@ data = data || {};
             dt++;
         } while (dt < 35);
         data = JSON.parse(localStorage.getItem("calendar"));
-        clDraw(genVar.curDate, genVar.todayDate, data);
+        clDraw(genVar.curDate, genVar.todayDate);
     };
 
-    calendar.addEvent = function () {
-        var form = document.getElementById(genVar.form),
-            date  = new Date(genVar.curDate.getTime());
+    calendar.editEvent = function () {
+        var date = new Date(genVar.curDate.getTime());
 
-        [].slice.call(document.querySelectorAll(genVar.clRow)).forEach(function (row, i) {
-            [].slice.call(row.querySelectorAll(genVar.clCol)).forEach(function (col, j) {
-                if (col.classList.contains('active')){
-                    var dt = 0;
-                    do {
-                        date.setDate(date.getDate() + 1);
-                        dt++;
-                    } while (dt < (i*7)+j);
-
-                }
-            })
-        });
-
-        tempData.id = parseInt(date.getTime(),10);
-
-        if (form.getElementsByClassName(genVar.form + '-title')[0].value)
-            tempData.title = form.getElementsByClassName(genVar.form + '-title')[0].value.toString();
+        findColDate(date);
+        tempData.id = parseInt(date.getTime(), 10);
+        if (editForm.title[0].value)
+            tempData.title = editForm.title[0].value.toString();
         else tempData.title = 'Заголовок пуст';
-
-        if (form.getElementsByClassName(genVar.form + '-date')[0].value)
-            tempData.date = form.getElementsByClassName(genVar.form + '-date')[0].value;
-        else tempData.date = form.getElementsByClassName(genVar.form + '-date')[0].getAttribute('placeholder').toString();
-
-        tempData.participants = form.getElementsByClassName(genVar.form + '-participants')[0].value.toString();
-        tempData.description = form.getElementsByClassName(genVar.form + '-description')[0].value.toString();
+        if (editForm.date[0].value)
+            tempData.date = editForm.date[0].value;
+        else tempData.date = editForm.date[0].getAttribute('placeholder').toString();
+        tempData.participants = editForm.participants[0].value.toString();
+        tempData.description = editForm.description[0].value.toString();
 
         data[tempData.id] = tempData;
-
         localStorage.setItem("calendar", JSON.stringify(data));
 
         hdBlockClose();
 
         data = JSON.parse(localStorage.getItem("calendar"));
-        //console.log(data);
-        clDraw(genVar.curDate, genVar.todayDate, data);
+        clDraw(genVar.curDate, genVar.todayDate);
     };
 
-    calendar.addEventShort = function () {
+    calendar.addEvent = function () {
+        var str = (addForm.title[0].value).toString();
+        str = str.split(',');
+        var strDate = str[0].split(' '),
+            strTime = str[1],
+            strTitle = str[2],
+            date  = new Date(strDate[2] + ', ' + (find(genVar.monthList, strDate[1])+1) + ', ' + strDate[0]);
+
+        tempData.id = parseInt(date.getTime(), 10);
+
+        if (strTitle == undefined)
+            tempData.title = 'Заголовок пуст';
+        else tempData.title = str[2];
+
+        if (strTime == undefined) tempData.date = strDate[0] + ', ' + strDate[1] + ', ' + strDate[2];
+        else tempData.date = strDate[0] + ', ' + strDate[1] + ', ' + strDate[2] + ', ' + strTime;
+
+        data[tempData.id] = tempData;
+        localStorage.setItem("calendar", JSON.stringify(data));
+
         hdBlockClose();
+
+        data = JSON.parse(localStorage.getItem("calendar"));
+        clDraw(genVar.curDate, genVar.todayDate);
     };
 
     calendar.removeEvent = function () {
-        var date  = new Date(genVar.curDate.getTime());
-
-        [].slice.call(document.querySelectorAll(genVar.clRow)).forEach(function (row, i) {
-            [].slice.call(row.querySelectorAll(genVar.clCol)).forEach(function (col, j) {
-                if (col.classList.contains('active')){
-                    var dt = 0;
-                    do {
-                        date.setDate(date.getDate() + 1);
-                        dt++;
-                    } while (dt < (i*7)+j);
-
-                }
-            })
-        });
+        var date = new Date(genVar.curDate.getTime());
+        findColDate(date);
 
         data = JSON.parse(localStorage.getItem("calendar"));
         for (var x in data) {
-            if (data[x].id == parseInt(date.getTime(),10)) {
+            if (data[x].id == parseInt(date.getTime(), 10)) {
                 delete data[x];
                 localStorage.setItem("calendar", JSON.stringify(data));
             }
         }
         data = JSON.parse(localStorage.getItem("calendar"));
-        clDraw(genVar.curDate, genVar.todayDate, data);
+        clDraw(genVar.curDate, genVar.todayDate);
         hdBlockClose();
     };
 
     eventHandling(genVar.trigger, genVar.block, genVar.close, genVar.clBlock);
 
 })(calendar, data);
+
+
+function addEventKeyDown ()
+{
+    if (event.keyCode == 13)
+    {
+        event.cancelBubble = true;
+        event.returnValue = false;
+        calendar.addEvent();
+    }
+}
